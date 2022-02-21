5ae5ed95afc35ca6009831183802e966f683fa81
StoutXuzzo <stoutxuzzo@gmail.com>
2022-02-21 12:40:03 +0100

    Security groups added

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        'security/race_security.xml',
         'security/ir.model.access.csv',
         'views/car_view.xml',
         'views/circuit_view.xml',
